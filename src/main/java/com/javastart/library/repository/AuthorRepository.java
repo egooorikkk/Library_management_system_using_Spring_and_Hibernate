3715700b9ece66d2e91c0c9219e05f969806e139
egooorikkk <egorsius0601@gmail.com>
2026-01-14 14:26:37 +0300

    2 repository interfaces created

---
+++ src/main/java/com/javastart/library/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.javastart.library.repository;
+
+import com.javastart.library.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
